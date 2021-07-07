39d5cc0e0c5cc540697a49a89ad8c41ee9090d60
Victor_Carvalho <vic.carva01@gmail.com>
2021-07-07 14:37:57 -0300

    CursoCsharp

---
+++ CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace CursoCSharp.Fundamentos
+{
+    class PrimeiroPrograma
+    {
+        static void  Executar()
+        {
+            Console.Write("Primeiro ");
+
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou");
+        }
+
+    }
+}
